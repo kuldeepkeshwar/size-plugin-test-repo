24c68226ff544b01ccc925eec4e27961622bafc8
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-06-25 23:52:57 +0530

    added config for size-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 const SizePlugin = require("./vendor/size-plugin");
 const fs = require("fs");
 module.exports = {
-  mode:"production",  
+  mode:process.env.NODE_ENV||"development",  
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins:[
-    new SizePlugin({writeToDisk:true})
+    new SizePlugin()
   ]
 };
