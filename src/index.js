f52b7dab7f459af52e5993d6e49b901c6f2447bc
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-08-12 20:14:15 +0530

    update webpack version:5

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-import _ from 'lodash';
+function component() {
+  const element = document.createElement("div");
 
+  const button = document.createElement("button");
+  const br = document.createElement("br");
 
+  button.innerHTML = "Click me and look at the console!";
+  (async () => {
+    const _ = await import(/* webpackChunkName: "lodash" */ "lodash");
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  })();
+  element.appendChild(br);
+  element.appendChild(button);
 
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      const print = module.default;
 
- function component() {
-    const element = document.createElement('div');
+      print();
+    });
 
-   const button = document.createElement('button');
-   const br = document.createElement('br');
+  return element;
+}
 
-   button.innerHTML = 'Click me and look at the console!';
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.appendChild(br);
-   element.appendChild(button);
-
-   // Note that because a network request is involved, some indication
-   // of loading would need to be shown in a production-level site/app.
-   button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-     const print = module.default;
-
-     print();
-   });
-
-    return element;
-  }
-
- document.body.appendChild(component());
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
   mode:process.env.NODE_ENV||"development",  
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins:[
-    //new SizePlugin()
-    new SizePlugin({filename:'size-plugin-browser.json'}),
-    new SizePlugin({filename:'size-plugin-server.json'})
+    new SizePlugin()
+    // new SizePlugin({filename:'size-plugin-browser.json'}),
+    // new SizePlugin({filename:'size-plugin-server.json'})
   ]
 };
 
